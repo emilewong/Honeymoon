9163c19282146fa75f9ee9b3f0e7bec735fa337a
Emile Wong <emilewong@outlook.com>
2021-07-09 01:19:22 +0800

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Emile Wong on 9/7/2021.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
